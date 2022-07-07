896d277fef79f38b15e9d44db232786dcabcecec
Olivier Lartillot <olivier.lartillot@gmail.com>
2022-07-07 21:33:13 +0200

    add datas in an array and a Class named Personnages

---
+++ Personnages.php
@@ -0,0 +1,40 @@
+<?php
+
+class Personnages 
+{
+
+    public $name;
+    public $image_marqueur;
+    public $vitesseMinimum;
+    public $vitesseMaximum;
+    public $forceMinimum;
+    public $forceMaximum;
+    public $fatigueApresFrappeMinimum;
+    public $fatigueApresFrappeMaximum;
+    public $deApresFrappeMinimum;
+    public $deApresFrappeMaximum;
+
+
+    public function __construct($name = 'default', string $image_marqueur, int $vitesseMinimum, int $vitesseMaximum, int $forceMinimum, int $forceMaximum, $fatigueApresFrappeMinimum, $fatigueApresFrappeMaximum, $deApresFrappeMinimum, $deApresFrappeMaximum)
+    {
+        $this->name = $name;
+        $this->image_marqueur = $image_marqueur;
+        $this->vitesseMinimum = $vitesseMinimum;
+        $this->vitesseMaximum = $vitesseMaximum;
+        $this->forceMinimum = $forceMinimum;
+        $this->forceMaximum = $forceMaximum;
+        $this->fatigueApresFrappeMinimum = $fatigueApresFrappeMinimum;
+        $this->fatigueApresFrappeMaximum = $fatigueApresFrappeMaximum;
+        $this->deApresFrappeMinimum = $deApresFrappeMinimum;
+        $this->deApresFrappeMinimum = $deApresFrappeMinimum;
+        $this->deApresFrappeMaximum = $deApresFrappeMaximum;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+
+
+}
+
---
+++ tableau_persos.php
@@ -0,0 +1,18 @@
+<?php
+require_once "Personnages.php";
+$persoDatas = [
+    1 => new Personnages(
+        'Morse', 'morse.jpg', 2, 6, 15, 30, 2,12, 1,3 
+    ),
+    2 => new Personnages(
+        'Speedy', 'speedy.jpg', 3, 6, 10, 22, 0, 8, 1,2 
+    ),
+
+
+];
+
+
+
+    
+    
+ 
